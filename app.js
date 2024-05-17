2fa989f7de747f4db2aee3aa15c2329f721dd064
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2024-05-17 08:36:27 +0200

    Config mongodb

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://UserRW:test123@cluster0.taw3ysv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !')
+);
+
+app.use(express.json());
+
 app.use((req, res, next) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
 module.exports = app;
