11658b9d4020cf4a3bcd139122b4a80bcb657b1b
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2024-05-30 23:13:40 +0200

    Commentaire + modif clé token

--- controllers/user.js
+++ controllers/user.js
@@ -3,13 +3,17 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
+//Controller de la route POST "signup"
 exports.signup = (req, res, next) => {
+    //Hashage du mot de passe
     bcrypt.hash(req.body.password, 10)
       .then(hash => {
+        //Ajout de l'email et du hash dans une const avec le modèle "User"
         const user = new User({
           email: req.body.email,
           password: hash
         });
+        //Enregistrement de cette const dans la BDD
         user.save()
           .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
           .catch(error => res.status(400).json({ error }));
@@ -17,22 +21,26 @@ exports.signup = (req, res, next) => {
       .catch(error => res.status(500).json({ error }));
 };
 
+//Controller de la route POST "login"
 exports.login = (req, res, next) => {
+    //Recherche de l'email en BDD
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
             }
+            //Comparaison du mot de passe avec celui en BDD
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
                         return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
                     }
+                    //Renvoi du response avec le token
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
+                            'CyhE3KF8CB',
                             { expiresIn: '24h' }
                         )
                     });
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,18 @@
 const jwt = require('jsonwebtoken');
- 
+
+//Middelware d'authentification
 module.exports = (req, res, next) => {
-   try {
-        //console.log(req.headers.authorization)
-       const token = req.headers.authorization.split(' ')[1];
-       const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-       const userId = decodedToken.userId;
-       req.auth = {
-           userId: userId
-       };
-	next();
-   } catch(error) {
-       res.status(401).json({ error });
-   }
+    try {
+        //Vérification du token envoyé par le front-end
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'CyhE3KF8CB');
+        //Ajout du userId dans la requète après décodage
+        const userId = decodedToken.userId;
+        req.auth = {
+            userId: userId
+        };
+	    next();
+    } catch(error) {
+        res.status(401).json({ error });
+    }
 };
